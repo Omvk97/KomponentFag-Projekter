b25bc8b69a96d67f8a56e999048f58f043430362
Oliver Marco van Komen <olvan18@student.sdu.dk>
2020-03-19 11:26:24 +0100

    Bundle Context API Enemy

--- 06-OSGiLab/Enemy/src/main/java/dk/sdu/mmmi/cbse/enemy/Activator.java
+++ 06-OSGiLab/Enemy/src/main/java/dk/sdu/mmmi/cbse/enemy/Activator.java
@@ -5,8 +5,11 @@
  */
 package dk.sdu.mmmi.cbse.enemy;
 
+import dk.sdu.mmmi.cbse.common.services.IEntityProcessingService;
+import dk.sdu.mmmi.cbse.common.services.IGamePluginService;
 import org.osgi.framework.BundleActivator;
 import org.osgi.framework.BundleContext;
+import org.osgi.framework.ServiceRegistration;
 
 /**
  *
@@ -14,12 +17,22 @@ import org.osgi.framework.BundleContext;
  */
 public class Activator implements BundleActivator {
     
+    private EnemyControlSystem controlSystem = new EnemyControlSystem();
+    private EnemyPlugin plugin = new EnemyPlugin();
+    
+    private ServiceRegistration controlSystemServiceRegistration;
+    private ServiceRegistration enemyPluginServiceRegistration;
+    
     public void start(BundleContext context) throws Exception {
         //TODO add activation code here
+        controlSystemServiceRegistration = context.registerService(IEntityProcessingService.class, controlSystem, null);
+        enemyPluginServiceRegistration = context.registerService(IGamePluginService.class, plugin, null);
     }
     
     public void stop(BundleContext context) throws Exception {
         //TODO add deactivation code here
+        controlSystemServiceRegistration.unregister();
+        enemyPluginServiceRegistration.unregister();
     }
     
 }
